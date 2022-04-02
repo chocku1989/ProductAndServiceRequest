6ca70c3c5532e462a815cc5f442579ed372dbe41
chocku1989 <chocku1989@gmail.com>
2022-04-02 21:31:30 +0530

    First Project

---
+++ productandservice/src/main/java/com/training/service/productandservice/ProductandserviceApplication.java
@@ -0,0 +1,13 @@
+package com.training.service.productandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductandserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductandserviceApplication.class, args);
+	}
+
+}
---
+++ productandservice/src/test/java/com/training/service/productandservice/ProductandserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.service.productandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductandserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
